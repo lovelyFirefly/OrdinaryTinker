e130dddf4947149bec1189787a3ec767bdff6b90
lovelyfirefly <1494932845@qq.com>
2024-12-28 21:55:48 +0800

    初始化

---
+++ src/main/java/com/hoshino/solidarytinker/solidarytinker.java
@@ -0,0 +1,9 @@
+package com.hoshino.solidarytinker;
+
+import net.minecraftforge.fml.common.Mod;
+
+
+@Mod(solidarytinker.MODID)
+public class solidarytinker {
+    public static final String MODID = "solidarytinker";
+}
