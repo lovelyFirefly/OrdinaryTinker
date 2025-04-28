6db9cf9a4db3671a6c45ccf46f7647f195869b90
lovelyFirefly <1494932845@qq.com>
2025-04-28 18:16:46 +0800

    盔甲信息数据包修正,来自牢蘑的stat信息补全

--- src/main/java/com/hoshino/ordinarytinker/Context/Item/Tool/OrdinaryTinkerArmorDefinitions.java
+++ src/main/java/com/hoshino/ordinarytinker/Context/Item/Tool/OrdinaryTinkerArmorDefinitions.java
@@ -1,10 +1,11 @@
 package com.hoshino.ordinarytinker.Context.Item.Tool;
 
+import com.hoshino.ordinarytinker.OrdinaryTinker;
 import net.minecraft.sounds.SoundEvents;
 import slimeknights.tconstruct.library.tools.definition.ModifiableArmorMaterial;
 
-import static slimeknights.tconstruct.TConstruct.getResource;
+
 
 public class OrdinaryTinkerArmorDefinitions {
-    public static final ModifiableArmorMaterial FLUID_PLATE = ModifiableArmorMaterial.create(getResource("fluid_plate"), SoundEvents.BUCKET_EMPTY);
+    public static final ModifiableArmorMaterial FLUID_PLATE = ModifiableArmorMaterial.create(OrdinaryTinker.getResource("fluid_plate"), SoundEvents.BUCKET_EMPTY);
 }
