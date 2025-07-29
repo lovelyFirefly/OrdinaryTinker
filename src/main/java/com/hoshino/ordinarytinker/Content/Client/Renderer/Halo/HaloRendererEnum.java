2d2c2cf92ef20b3960dbc43fb9f313a2f6478da0
lovelyFirefly <1494932845@qq.com>
2025-07-29 20:37:31 +0800

    依赖项修复

--- src/main/java/com/hoshino/ordinarytinker/Content/Client/Renderer/Halo/HaloRendererEnum.java
+++ src/main/java/com/hoshino/ordinarytinker/Content/Client/Renderer/Halo/HaloRendererEnum.java
@@ -1,11 +1,11 @@
 package com.hoshino.ordinarytinker.Content.Client.Renderer.Halo;
 
 import com.hoshino.ordinarytinker.Content.Util.ModifierLevel;
+import com.hoshino.ordinarytinker.OrdinaryTinker;
 import com.hoshino.ordinarytinker.Register.OrdinaryTinkerModifier;
 import com.mojang.blaze3d.vertex.PoseStack;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.player.Player;
-import nking.tinkerorthodox.Tinkerorthodox;
 import slimeknights.tconstruct.library.modifiers.Modifier;
 import slimeknights.tconstruct.library.modifiers.ModifierId;
 
@@ -24,7 +24,7 @@ public enum HaloRendererEnum implements HaloRendererUtil {
 
     HaloRendererEnum(Supplier<Modifier> modifierSupplier, String textureName) {
         this.modifierSupplier = modifierSupplier;
-        this.texture = Tinkerorthodox.getResource("textures/halo/" + textureName + ".png");
+        this.texture = OrdinaryTinker.getResource("textures/halo/" + textureName + ".png");
     }
 
     public Modifier getModifier() {
