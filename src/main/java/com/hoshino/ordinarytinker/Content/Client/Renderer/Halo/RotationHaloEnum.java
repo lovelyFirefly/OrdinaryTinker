eb3931bb07ea9c45ce70c84cb688a522f84ad79c
lovelyFirefly <1494932845@qq.com>
2025-07-29 20:37:49 +0800

    依赖项修复

--- src/main/java/com/hoshino/ordinarytinker/Content/Client/Renderer/Halo/RotationHaloEnum.java
+++ src/main/java/com/hoshino/ordinarytinker/Content/Client/Renderer/Halo/RotationHaloEnum.java
@@ -1,11 +1,11 @@
 package com.hoshino.ordinarytinker.Content.Client.Renderer.Halo;
 
 import com.hoshino.ordinarytinker.Content.Util.ModifierLevel;
+import com.hoshino.ordinarytinker.OrdinaryTinker;
 import com.hoshino.ordinarytinker.Register.OrdinaryTinkerModifier;
 import com.mojang.blaze3d.vertex.PoseStack;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.player.Player;
-import nking.tinkerorthodox.Tinkerorthodox;
 import slimeknights.tconstruct.library.modifiers.Modifier;
 import slimeknights.tconstruct.library.modifiers.ModifierId;
 
@@ -19,7 +19,7 @@ public enum RotationHaloEnum implements HaloRendererUtil {
 
     RotationHaloEnum(Supplier<Modifier> modifierSupplier, String textureName) {
         this.modifierSupplier = modifierSupplier;
-        this.texture = Tinkerorthodox.getResource("textures/halo/" + textureName + ".png");
+        this.texture = OrdinaryTinker.getResource("textures/halo/" + textureName + ".png");
     }
 
     public Modifier getModifier() {
